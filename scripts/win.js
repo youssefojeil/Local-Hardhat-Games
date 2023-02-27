a1f7d20106dc0bcc6b871b2cc5310e29bccebbdf
unknown <youssef.ojeil@hotmail.com>
2023-02-27 11:02:14 -0500

    win game 5

--- scripts/win.js
+++ scripts/win.js
@@ -1,6 +1,6 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
-const contractName = "Game4";
+const gameAddr = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+const contractName = "Game5";
 
 async function main() {
   // attach to the game
@@ -19,10 +19,21 @@ async function main() {
   // const tx = await game.win(45);
 
   // game 4
-  const tx = await game.win(-200);
+  //const tx = await game.win(-200);
+
+  // game 5
+
+  const tx = await game.giveMeAllowance(10000);
+  await tx.wait();
+
+  const tx2 = await game.mint(10000);
+  await tx2.wait();
+
+  const tx3 = await game.win();
+
   // did you win? Check the transaction receipt!
   // if you did, it will be in both the logs and events array
-  const receipt = await tx.wait();
+  const receipt = await tx3.wait();
   console.log(receipt);
 }
 
